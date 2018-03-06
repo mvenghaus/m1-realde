f74bc013f0cbe4a8425cdafae7a67c10a0509596
Marcus Venghaus <info@inklammern.de>
2018-03-06 08:24:51 +0100

    new api url (real.de)

--- app/code/local/Inkl/RealDe/Helper/Api/Client.php
+++ app/code/local/Inkl/RealDe/Helper/Api/Client.php
@@ -7,6 +7,7 @@ class Inkl_RealDe_Helper_Api_Client extends Mage_Core_Helper_Abstract
 	public function build($storeId = null)
 	{
 		return ClientBuilder::create()
+            ->setBaseUrl('https://www.real.de/api/v1/')
 			->setClientKey(Mage::helper('inkl_realde/config_api')->getClientKey($storeId))
 			->setClientSecret(Mage::helper('inkl_realde/config_api')->getClientSecret($storeId))
 			->build();
